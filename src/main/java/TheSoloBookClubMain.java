b4f8342fad40f807a224f5bd72575fb4ffa17804
ce691 <email@email.com>
2018-09-05 00:18:26 -0500

    GUI 1 in progress :)

--- src/main/java/TheSoloBookClubMain.java
+++ src/main/java/TheSoloBookClubMain.java
@@ -1,2 +1,8 @@
+import javax.swing.*;
+
 public class TheSoloBookClubMain {
+    private JTextField titleSearchTextField;
+    private JButton findNextReadButton;
+    private JButton bookAddLookUpButton;
+    private JLabel titleLabel;
 }
